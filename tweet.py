aeb4f1bb9833966f82554000440b42c1b59de074
Jara <jaraguta03@gmail.com>
2022-11-19 11:23:01 +0300

    add limit_handler function and follow back a choosen follower

--- tweet.py
+++ tweet.py
@@ -1,5 +1,5 @@
 import tweepy
-
+import time
 auth = tweepy.OAuth1UserHandler(
     consumer_key, consumer_secret, access_token, access_token_secret
 )  # Api key = consumer_key , Api secret key = consumer_secret
@@ -12,8 +12,17 @@ print(myself.name)  # prints your name.
 print(myself.screen_name)  # prints your twitter handle
 print(myself.followers_count)
 
+
+def limit_handler(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.TooManyRequests:
+            time.sleep(1000)
+
+
 # print out all the followers of our account
-for follower in (tweepy.Cursor(api.get_followers).items()):
+for follower in limit_handler(tweepy.Cursor(api.get_followers).items()):
     if follower.name == 'Usernamehere':
         print(follower.name)
-    
+        follower.follow()
